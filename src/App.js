7c8eaa4d1c8149f65461e9a772f09a867b63b9dd
ka52k <76809740+ka52k@users.noreply.github.com>
2021-03-16 12:55:21 +0900

    20210316－1変更

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/Searchinput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+// import { BrowserRouter } from "react-router-dom";
+// import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+// import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+// import { SearchInput } from "./components/molecules/Searchinput";
+// import { UserCard } from "./components/organisms/user/UserCard";
+// import { DefaultLayout } from "./components/templates/DefaultLayout";
+// import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
-
-const user = {
-  name: "KK",
-  image: "https://source.unsplash.com/OzAeZPNsLXk",
-  email: "kk123@kk.com",
-  phone: "090-1184-7382",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
 
 export default function App() {
   return (
-    <BrowserRouter>
-    <DefaultLayout>
-      <PrimaryButton>テスト</PrimaryButton>
-      {/* <PrimaryButton>検索</PrimaryButton> */}
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </DefaultLayout>
-    </BrowserRouter>
+// <BrowserRouter>
+//     <DefaultLayout>
+//       <PrimaryButton>テスト</PrimaryButton>
+//       {/* <PrimaryButton>検索</PrimaryButton> */}
+//       <SecondaryButton>検索</SecondaryButton>
+//       <br />
+//       <SearchInput />
+//       <UserCard user={user} />
+//     </DefaultLayout>
+// </BrowserRouter>
+    <Router />
   );
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,36 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/Searchinput";
+import { UserCard } from "../organisms/user/UserCard";
+const users = [...Array(10).keys()].map(((val) => {
+  return {
+    id: val,
+    name: `KK${val}`,
+    image: "https://source.unsplash.com/OzAeZPNsLXk",
+    email: "kk123@kk.com",
+    phone: "090-1184-7382",
+    company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+  }
+}))
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
+      <SearchInput />
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </SContainer>
   )
 }
 
 const SContainer = styled.div`
-  text-align: center;
+  /* text-align: center; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
 `;
